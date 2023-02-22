78a9aabddd64abcc61646a2b4ad88f5fc7089f18
mikehnzx <86700074+mikehnzx@users.noreply.github.com>
2023-02-22 20:41:25 +1300

    Update streamlit_app.py
    added streamlit import and title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
-
+import streamlit
 import snowflake.connector
 
+streamlit.title("This is a test website")
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
